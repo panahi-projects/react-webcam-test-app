09112213a48f3c7d539eca5153e0993fbcce8cb6
SAEED.PANAHI <panahi.projects@gmail.com>
2024-06-13 18:44:09 +0330

    fix bug

---
+++ app/components/Camera3.tsx
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+const Camera3 = () => {
+  const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const [videoUrl, setVideoUrl] = useState(null);
+
+  const handleStartCaptureClick = () => {
+    setCapturing(true);
+    setVideoUrl(null); // Clear previous video URL if any
+    (mediaRecorderRef as any).current = new MediaRecorder(
+      (webcamRef as any).current.stream,
+      {
+        mimeType: "video/webm",
+      }
+    );
+    (mediaRecorderRef as any).current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    (mediaRecorderRef as any).current.start();
+  };
+
+  const handleDataAvailable = ({ data }: { data: any }) => {
+    if (data.size > 0) {
+      setRecordedChunks((prev) => prev.concat(data));
+    }
+  };
+
+  const handleStopCaptureClick = () => {
+    (mediaRecorderRef as any).current.stop();
+    setCapturing(false);
+  };
+
+  const handleSaveVideo = () => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      setVideoUrl(url as any);
+      setRecordedChunks([]);
+    }
+  };
+
+  return (
+    <div>
+      <Webcam audio={true} ref={webcamRef} />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleSaveVideo}>Save Video</button>
+      )}
+      {videoUrl && (
+        <div>
+          <h3>Recorded Video:</h3>
+          <video src={videoUrl} controls autoPlay loop />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Camera3;
--- app/components/CameraComponent.tsx
+++ app/components/CameraComponent.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Camera from "./Camera";
 import Camera2 from "./Camera2";
+import Camera3 from "./Camera3";
 
 const CameraComponent = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,7 +13,7 @@ const CameraComponent = () => {
 
   if (!isMounted) {
     return null;
-  } else return <Camera2 />;
+  } else return <Camera3 />;
 };
 
 export default CameraComponent;
